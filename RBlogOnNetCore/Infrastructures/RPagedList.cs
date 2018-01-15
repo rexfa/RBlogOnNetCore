fadb6b6dfb79e00c1ecfe0ec2daf80d26b5c23b9
Rex Zhang <rex_fa@hotmail.com>
2018-01-15 23:12:42 +0800

    补充

---
+++ RBlogOnNetCore/Infrastructures/RPagedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RBlogOnNetCore.Infrastructures
+{
+    public class RPagedList
+    {
+    }
+}
